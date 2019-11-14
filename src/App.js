88b804fc460d069dd079d7d6522193f07a2fc9bf
rishikant42 <rshkntshrm@gmail.com>
2019-11-14 13:09:11 +0530

    updatex text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> & Hi
+          Learning CI/CD with docker, travis-ci, aws-elasticbeanstalk
         </p>
         <a
           className="App-link"
